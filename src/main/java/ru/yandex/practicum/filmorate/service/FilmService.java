50a5cfa3933c7683f602382274374c1ffa591bf5
Sergey Shihovcev <Shihovcev.sv@yandex.ru>
2025-07-18 17:12:50 +0300

    Check and Build

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,20 +12,24 @@ import java.util.stream.Collectors;
 @Service
 public class FilmService {
     private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
 
     @Autowired
-    public FilmService(FilmStorage filmStorage) {
+    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
         this.filmStorage = filmStorage;
+        this.userStorage = userStorage;
     }
 
     public void addLike(long filmId, long userId) {
         Film film = filmStorage.getFilmById(filmId).orElseThrow();
+        userStorage.getUserById(userId).orElseThrow(() -> new java.util.NoSuchElementException("Пользователь с id=" + userId + " не найден"));
         film.getLikes().add(userId);
         filmStorage.updateFilm(film);
     }
 
     public void removeLike(long filmId, long userId) {
         Film film = filmStorage.getFilmById(filmId).orElseThrow();
+        userStorage.getUserById(userId).orElseThrow(() -> new java.util.NoSuchElementException("Пользователь с id=" + userId + " не найден"));
         film.getLikes().remove(userId);
         filmStorage.updateFilm(film);
     }
